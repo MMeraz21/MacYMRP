6c16d95b22d465c6127c73ba3651c02a33a5cc18
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-06-20 20:57:04 -0500

    before crazy css changes

--- desktop-client/macymrp/electron.js
+++ desktop-client/macymrp/electron.js
@@ -13,6 +13,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
